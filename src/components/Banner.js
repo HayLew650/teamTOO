db18d3c74d7e7b6782104a8e9ceac35898d61dfa
Hayden Lewi <hfl127@hotmail.com>
2019-07-27 17:04:18 -0700

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div id="banner">
+    <h1>this is banner</h1>
+    <h2>Alpha</h2>
+					<p>Another fine responsive site template freebie by HTML5 UP.</p>
+					<ul class="actions special">
+						<li><a href="#" class="button primary">Sign Up</a></li>
+						<li><a href="#" class="button">Learn More</a></li>
+					</ul>
+      {/* <Header /> */}
+      {/* <banner /> */}
+      {/* <main /> */}
+      {/* <cta /> */}
+      {/* <footer /> */}
+    </div>
+  );
+}
+
+export default Banner;
