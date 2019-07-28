e6909bd2b21780be8e71749eb35993d9426cfeaa
Hayden Lewi <hfl127@hotmail.com>
2019-07-27 20:45:15 -0700

    Logic for intergrating api

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-
+import axios from "axios"
 
 class Banner extends Component {
     constructor() {
@@ -8,7 +8,56 @@ class Banner extends Component {
 
         }
       }
+      //
+      <body>
+    Metadata: <input type="file" value="metadata"><br>
+    Content: <input type="file" value="contentdata"><br>
+    <input type="button" onclick="upload()" value="Test">
+<script type="text/javascript">
+
+
+    upload = function() {
+
+
+
+        var formData = new FormData();
+        var metadataBlob = new Blob([$('input[type=file]')[0].files[0]],{type:"application/json"});
+        var contentBlob = new Blob([$('input[type=file]')[1].files[0]],{type:"text/plain"});
+        formData.append('data', metadataBlob, "metadata");
+        formData.append('cid_63apple', contentBlob, "contentdata");
+
+        $.ajax({
+            url: "https://api.yuuvis.io/dms/objects",
+            processData: false,
+            contentType: false,
+            cache: false,
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","{9e5ef43b87034ff5a9a9cdaa58914638}");
+            },
+            type: "POST",
+
+            data: formData
+        })
+        .done(function(data) {
+            alert("success");
+            console.log(data);
+        })
+        .fail(function() {
+            alert("error");
+        });
+    };
+</script>
+</body>
+//
       //  this.handleChange = this.handleChange.bind(this)
+      componentDidMount() { //
+         axios.get(`https://yuuvis.io/dms/objects`)
+           .then(res => {
+             const persons = res.data;
+             this.setState({ persons });
+           })
+       }
 
     render() {
         return (
